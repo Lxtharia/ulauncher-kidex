7681d804c46eb392841840f00c2546ccb7bc52ff
Lxtharia <maud-lin@gmx.de>
2025-08-25 01:41:32 +0200

    Add call to kidex-client and parsing of results.
    It just works.

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import json
 
 def parse_index_entry():
     pass
@@ -9,12 +12,27 @@ class KidexWarningException(Exception):
     pass
 
 class IndexEntry:
-    def __init__(self, basename: str, path: str, type):
-        self.basename = basename
+    def __init__(self, path: str, type):
+        self.basename = os.path.basename(path)
         self.path = path
         self.type = type
         pass
 
-def get_find_results(query_string: str) -> list[IndexEntry]:
-    raise KidexException("Kidex not running or something")
-    return []
+def get_find_results(query_string: str, limit: int = 100) -> list[IndexEntry]:
+    results = []
+    try:
+        process_result = subprocess.check_output(["kidex-client", "find", query_string])
+        result: list[dict[str,str]] = json.loads(process_result)
+        i = 0
+        for entry in reversed(result):
+            i += 1
+            if i >= limit:
+                break
+            results.append(IndexEntry(
+                entry['path'],
+                "dir" if entry['directory'] else "file"
+            ))
+    except subprocess.CalledProcessError:
+        raise KidexErrorException("Kidex not running or something")
+
+    return results
