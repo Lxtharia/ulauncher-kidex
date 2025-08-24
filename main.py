327231e6967c555049b6d910ffa92fb5477de94d
Lxtharia <maud-lin@gmx.de>
2025-08-25 00:46:41 +0200

    Add Action Menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 
@@ -11,19 +12,32 @@ class KidexExtension(Extension):
     def __init__(self):
         super().__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         items = []
+        data = {}
         for i in range(5):
             items.append(ExtensionResultItem(icon='images/icon.png',
                                              name='Item %s' % i,
                                              description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
 
         return RenderResultListAction(items)
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        data = event.get_data()
+        actions = [
+            ExtensionResultItem(
+                name="Copy",
+                on_enter=HideWindowAction()
+            ),
+        ]
+        return RenderResultListAction(actions)
 
 
 if __name__ == '__main__':
